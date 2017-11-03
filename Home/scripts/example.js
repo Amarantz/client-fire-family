ce8a155528f037386ce4e8a8e6198cf760d4fd60
Brie Miller <brieauty@gmail.com>
2017-11-02 20:04:32 -0600

    correct syntax to call the api, access has been denied to the api. using js in allProducts not example.js. removed comments in main.go

--- Home/scripts/example.js
+++ Home/scripts/example.js
@@ -1,6 +1,13 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.codecademy.com/", false);
-xhr.send();
+$(document).ready(
+    function (){
+        $("#clickbtn").click( function (){
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "http://www.google.com", false);
+            xhr.send();
 
-console.log(xhr.status);
-console.log(xhr.statusText);
+            console.log(xhr.status);
+            console.log(xhr.statusText);
+            $("#allProducts").text(xhr.status + " " + xhr.statusText)
+        });
+
+});
